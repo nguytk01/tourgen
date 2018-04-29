b77e3d0ed8712016f1e5cea84c506d651f5e6aff
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-04-29 13:30:53 -0400

    Tested meet suggestions for NewMainViewController. Issue #47

---
+++ tourgen/controller/src/test/java/tourgen/controller/BaseTestUtils.java
@@ -0,0 +1,33 @@
+package tourgen.controller;
+
+import org.junit.BeforeClass;
+
+import tourgen.model.Repository;
+import tourgen.model.SchoolManager;
+
+public class BaseTestUtils {
+	protected static SchoolManager schoolManager;
+	@BeforeClass
+	  public static void setUpBeforeClass() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+	    schoolManager = new SchoolManager();
+		schoolManager.initSchools();
+		clearRepositorySingleton();
+	  }
+	  
+	  private static void _clearRepositorySingleton() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+	    java.lang.reflect.Field field = null;
+	    field = Repository.class.getDeclaredField("instance");
+	    field.setAccessible(true);
+	    field.set(field, null);
+	    schoolManager = new SchoolManager();
+	    schoolManager.initSchools();
+	  }
+	  
+	  protected static void clearRepositorySingleton(){
+		  try {
+			  _clearRepositorySingleton();
+		  } catch (Exception e){
+			  
+		  }
+	  }
+}
--- tourgen/controller/src/test/java/tourgen/controller/NewMainViewControllerTest.java
+++ tourgen/controller/src/test/java/tourgen/controller/NewMainViewControllerTest.java
@@ -1,43 +1,94 @@
 package tourgen.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.Before;
 
+import tourgen.model.IoManager;
+import tourgen.model.Location;
 import tourgen.model.Meet;
+import tourgen.model.Repository;
+import tourgen.model.RepositoryInitialization;
+import tourgen.model.School;
 import tourgen.model.SchoolManager;
 import tourgen.model.Stage;
 import tourgen.model.StageType;
 import tourgen.model.Tournament;
 import tourgen.model.TournamentParticipants;
 
-public class NewMainViewControllerTest {
+public class NewMainViewControllerTest extends BaseTestUtils {
+	private School crownPoint;
+	private NewMainViewController controller;
+	private MainViewPanelMock leftPanelMock;
+	private MainViewPanelMock rightPanelMock;
 
-  @Test
-  public void testSetActiveTournament() {
-    MainViewPanelMock leftPanelMock = new MainViewPanelMock();
-    MainViewPanelMock rightPanelMock = new MainViewPanelMock();
-    Tournament tournament = new Tournament("", TournamentParticipants.GIRLS);
-    NewMainViewController controller = new NewMainViewController(leftPanelMock, rightPanelMock, null);
-    controller.setActiveTournament(tournament);
-    
-    assertEquals(tournament, leftPanelMock.getTournamentJustSet());
-    assertEquals(tournament, rightPanelMock.getTournamentJustSet());
-  }
-  
-  @Test
-  public void testGetSchoolManager(){
-	  SchoolManager manager = new SchoolManager();
-	  NewMainViewController controller = new NewMainViewController(null, null, manager);
-	  assertEquals(manager, controller.getSchoolManager());
-  }
+	@Before
+	public void setUp() {
+		clearRepositorySingleton();
+		RepositoryInitialization.init("tournamentDataForControllerTests.txt", Repository.getInstance1(), schoolManager);
+		Location crownPointLocation = new Location("n", "n", 12);
+		crownPointLocation.setLatitude(41.389664);
+		crownPointLocation.setLongitude(-87.359668);
+		crownPoint = new School("Crown Point", "Crown Point High School", "n", "n", 123, 123, true, true);
+		crownPoint.setSchoolLoc(crownPointLocation);
 
-  @Test
-  public void testGetAvailableSectionalMeets(){
-	  Tournament tournament = new Tournament("", TournamentParticipants.GIRLS);
-	  Stage stage = new Stage(StageType.SECTIONAL, "" ,"");
-	  tournament.addStage(stage);
-	  Meet meetA = new Meet(stage, null);  
-	  Meet meetB = new Meet(stage, null);  
-  }
+		leftPanelMock = new MainViewPanelMock();
+		rightPanelMock = new MainViewPanelMock();
+		controller = new NewMainViewController(leftPanelMock, rightPanelMock, null);
+	}
+
+	@Test
+	public void testSetActiveTournament() {
+		Tournament tournament = new Tournament("", TournamentParticipants.GIRLS);
+		controller.setActiveTournament(tournament);
+		assertEquals(tournament, leftPanelMock.getTournamentJustSet());
+		assertEquals(tournament, rightPanelMock.getTournamentJustSet());
+	}
+
+	@Test
+	public void testGetSchoolManager() {
+		NewMainViewController controller = new NewMainViewController(null, null, schoolManager);
+		assertEquals(schoolManager, controller.getSchoolManager());
+	}
+
+	@Test
+	public void testGetAvailableSectionalMeets() {
+		java.util.List<School> schoolList = schoolManager.getSchoolList();
+
+		School crownPointFromList = null;
+		for (School school : schoolList) {
+			if (school.getName().equals("Crown Point")) {
+				crownPoint = school;
+			}
+		}
+		controller.setActiveTournament(Repository.getInstance1().getGirlList().get(0));
+		java.util.List<Meet> meetList = controller.getAvailableSectionalMeets(
+				Repository.getInstance1().getGirlList().get(0).getStageList().get(1).getMeetList().get(0),
+				crownPoint);
+	    assertEquals(1, meetList.size());
+		assertEquals("Hammond Gavit", meetList.get(0).getHostSchool().getDisplayName());
+	}
+	
+	@Test
+	public void testNullTournamentGetAvailableSectionalMeets() {
+		java.util.List<School> schoolList = schoolManager.getSchoolList();
+
+		School crownPointFromList = null;
+		for (School school : schoolList) {
+			if (school.getName().equals("Crown Point")) {
+				crownPoint = school;
+			}
+		}
+		java.util.List<Meet> meetList = controller.getAvailableSectionalMeets(
+				Repository.getInstance1().getGirlList().get(0).getStageList().get(1).getMeetList().get(0),
+				crownPoint);
+	    assertEquals(null, meetList);
+	
+	}
+
+	@Test
+	public void testSaveCurrentTournamentClicked() {
+
+	}
 }
